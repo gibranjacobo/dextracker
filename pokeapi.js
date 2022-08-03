6507b0ef6ebe8fa828295f0332d1723d13be53fe
GibranJacobo <gibran@gibranjacobo.com>
2022-08-02 20:55:04 -0500

    Added Stylesheet

--- pokeapi.js
+++ pokeapi.js
@@ -1,20 +1,23 @@
-import fetch from "node-fetch";
+console.log("hello world");
 
-let pokeres = {};
-let pokenum = Math.floor(Math.random() * (898 - 1 + 1)) + 1;
+// funcion para cuando la llamada es exitosa
+function exito() {
+    var datos = JSON.parse(this.responseText); //convertir a JSON
+    console.log(datos.pokemon_entries);
+    for (let i = 0; i < datos.pokemon_entries.length; i++) {
+        //console.log(datos.results[i].name);
+        document.getElementById('pokelist').innerHTML += '<li class="pokemon-entry">' + datos.pokemon_entries[i].pokemon_species.name + '</li>';
+    }
+    
+}
 
-fetch('https://pokeapi.co/api/v2/pokemon/' + pokenum)
-.then(response => response.json())
-.then(data => {
-    console.log(data.name);
-    for (let i = 0; i < data.types.length; i++) {
-        console.log("Type " + (i+1) + ": " + data.types[i].type.name);
-    }
-    for (let i = 0; i < data.abilities.length; i++) {
-        console.log("Ability " + (i+1) + ": " + data.abilities[i].ability.name);
-    }
-    for (let i = 0; i < data.stats.length; i++) {
-        console.log(data.stats[i].stat.name + ": " + data.stats[i].base_stat);
-    }
-})
-.catch(err => console.log('Solicitud fallida', err));
+// funcion para la llamada fallida
+function error(err) {
+    console.log('Solicitud fallida', err); //los detalles en el objecto "err"
+}
+
+var xhr = new XMLHttpRequest(); //invocar nueva instancia de XMLHttpRequest
+xhr.onload = exito; // llamar a la funcion exito si exitosa
+xhr.onerror = error;  // llamar a la funcion error si fallida
+xhr.open('GET', 'https://pokeapi.co/api/v2/pokedex/2'); // Abrir solicitud GET
+xhr.send(); // mandar la solicitud al vervidor.
